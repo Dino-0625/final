dffc1f324f0cce603afefeb096f6aefb704cc4b9
jackson <jiechun.lai@gmail.com>
2020-12-28 18:46:15 +0800

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by User02 on 2020/12/28.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
